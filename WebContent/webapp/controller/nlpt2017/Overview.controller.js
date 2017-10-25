c33473f8aecfed40bf4542e36a42753da32271a1
Alexander Seibel <a.seibel90@gmx.de>
2017-10-25 14:47:11 +0200

    added 2017 overview to toolpage

---
+++ WebContent/webapp/controller/nlpt2017/Overview.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([ "sap/ui/core/mvc/Controller", "sap/m/MessageBox" ], function(Controller, MessageBox) {
+	"use strict";
+
+	return Controller.extend("com.nlpt.app.controller.nlpt2017.Overview", {
+
+		onInit : function() {
+			appController = this;
+		},
+
+	})
+
+});
