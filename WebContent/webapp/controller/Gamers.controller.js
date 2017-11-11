28469fb7678d1f84727ce6eb34d71567abc545f6
Alexander Seibel <a.seibel90@gmx.de>
2017-11-11 18:24:02 +0100

    added Gamer view and Sponsors views

--- WebContent/webapp/controller/Gamers.controller.js
+++ WebContent/webapp/controller/Gamers.controller.js
@@ -1,14 +1,22 @@
-sap.ui.define([
-		"com/nlpt/app/controller/BaseController",
-		"sap/ui/model/json/JSONModel"
-	], function (BaseController, JSONModel) {
-		"use strict";
+sap.ui.define([ "com/nlpt/app/controller/BaseController", "sap/ui/model/json/JSONModel" ], function(BaseController, JSONModel) {
+	"use strict";
 
-		return BaseController.extend("com.nlpt.app.controller.Gamers", {
+	return BaseController.extend("com.nlpt.app.controller.Gamers", {
 
 		onInit : function() {
-
+			var router = this.getRouter();
+			router.getRoute("gamers").attachMatched(this._onRouteMatched, this);
 		},
+
+		_onRouteMatched : function(oEvent) {
+			var parameter;
+			var that = this;
+			this._refreshOverviewModel("0000");
+		},
+		
+		_refreshOverviewModel : function(year) {
+			this.getView().getModel("Gamers").loadData("../../nlpt_php/Gamer.php?year=" + year);
+		}
 
 	});
 
